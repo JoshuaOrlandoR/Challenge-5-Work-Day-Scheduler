d3b7e206d7e90b23efbb53ebb043f9708873bcd1
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2022-11-11 14:43:58 -0500

    Third commit - added all timeslots to HTML, tried to update past/present/future classes but my solution failed, commiting for a  backup if needed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+var timeNow = moment().hour()
+// var agendaRow = $(".agendaRow")
 // Current Time displayed in <p> currentDay
 function liveTime() {
     $("#currentDay").text(moment().format("MMMM, Do, YYYY, h:mm:ss A"));
@@ -7,3 +9,31 @@ function liveTime() {
 liveTime();
 setInterval(liveTime,1000);
 
+//Changing colors depending on time - This way did not work the way I intended, come back to this 
+
+// function hourStatus() {
+//     $(".agendaRow").each (function () {
+//         var agendaTime = parseInt($(".agendaRow").attr("id").split("time")[1]); - thought this was pretty clever though
+//         console.log (agendaTime, timeNow)
+        
+//         if (agendaTime < timeNow) {
+//             $("agendaRow").addClass("past");
+//         }
+//     })
+// }
+
+// hourStatus();
+
+
+
+
+
+
+
+
+
+//Checking to see if buttons work 
+$(".saveBtn").on("click", function(){
+    var timeNow = moment().hour();
+    console.log(timeNow);
+});
